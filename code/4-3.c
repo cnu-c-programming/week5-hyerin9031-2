4b4c19ed3aea717b349da1c6bce671671fefc1e7
hyerin9031 <148173524+hyerin9031@users.noreply.github.com>
2026-04-09 13:54:47 +0900

    Update 4-3.c

---
+++ code/4-3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int arr[5] = {10, 20, 30, 40, 50};
+    int size = 5;
+    int index;
+
+    scanf("%d", &index);
+
+    if(index < size){
+        printf("%d", arr[index]);
+    }
+    else{
+        printf("Error!");
+    }
+
+    return 0;
+}
